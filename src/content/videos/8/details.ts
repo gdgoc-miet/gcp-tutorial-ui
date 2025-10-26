89ea76f1d5f08efee1314d8eef700e90a5d8b756
Radhey Kalra <radheykalra901@gmail.com>
2025-10-27 00:53:37 +0530

    updated wrong link

--- src/content/videos/8/details.ts
+++ src/content/videos/8/details.ts
@@ -3,7 +3,6 @@ import type { LessonDetails } from '@/lib/types';
 export const details: LessonDetails = {
   title: 'Cloud Storage: Qwik Start - CLI/SDK',
   sidebarTitle: 'Lab 2.2',
-  videoId: 'Ke-Mi0WrXlw',
+  videoId: '6pk8o1EGRoI',
   description: `In this hands-on lab you will learn how to use the Google Cloud command line to create a storage bucket, upload objects to the bucket, create folders and subfolders in the bucket, and make objects in a storage bucket publicly accessible.`,
 };
-
