f125adf6c56126855feba5b2a76772192e5c70ca
Tobi-maru <sharmasheershak26@gmail.com>
2025-11-04 21:47:32 +0530

    content(videos): add details and notes for third module

---
+++ src/content/videos/12/details.ts
@@ -0,0 +1,9 @@
+import type { LessonDetails } from '@/lib/types';
+
+export const details: LessonDetails = {
+  title: 'Cloud Scheduler',
+  sidebarTitle: 'Lab 3.2',
+  videoId: '0ftS2-89Kzo',
+  description: `Create a recurring cloud scheduler that specifies Pub/sub topic as job target`,
+};
+
---
+++ src/content/videos/13/details.ts
@@ -0,0 +1,9 @@
+import type { LessonDetails } from '@/lib/types';
+
+export const details: LessonDetails = {
+  title: 'Challange lab',
+  sidebarTitle: 'Lab 3.3',
+  videoId: 'rIEQsK1cGX0',
+  description: `Challange lab to test your understanding `,
+};
+
